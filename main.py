295daf84c372dc7a43ed7f1cc63d6ce2490790db
t4t5u0 <ymmtryk0902@gmail.com>
2021-03-16 11:12:15 +0900

    [add] main 関数追加

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
+def main():
 
-path = Path.cwd().glob('**/config.ini')
-config = configparser.ConfigParser()
-config.read(path)
-TOKEN = config['TOKEN']['token']
+    path = Path.cwd().glob('**/config.ini')
+    config = configparser.ConfigParser()
+    config.read(path)
+    TOKEN = config['TOKEN']['token']
 
-prefix = '/'
-bot = Bot(command_prefix=prefix)
-bot.load_extension('cog.auth_bot')
+    prefix = '/'
+    bot = Bot(command_prefix=prefix)
+    bot.load_extension('cog.auth_bot')
+    bot.run(TOKEN)
 
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    main()
