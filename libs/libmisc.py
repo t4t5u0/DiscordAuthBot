abe56fee67840dffc6f90d1b676722c524e39fba
t4t5u0 <ymmtryk0902@gmail.com>
2021-04-08 03:19:46 +0900

    [add] token の発行と、比較をする関数

--- libs/libmisc.py
+++ libs/libmisc.py
@@ -1,11 +1,15 @@
+import hashlib
+import hmac
 import time
-from hashlib import shake_128
 from datetime import datetime
+from hashlib import shake_128
 
 
 def create_token() -> str:
-    t = datetime.timestamp(datetime.now()).__int__().__str__()
-    pass
+    t = datetime.timestamp(datetime.now()).__int__().__str__().encode()
+    h = hashlib.sha256(t).hexdigest()[:8]
+    return h
 
-def compare_token():
-    pass
+
+def compare_token(target1: str, target2: str) -> bool:
+    return hmac.compare_digest(target1, target2)
