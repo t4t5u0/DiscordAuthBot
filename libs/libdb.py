67c22d64e2913d634d5bda94746b677c4d7faa32
t4t5u0 <ymmtryk0902@gmail.com>
2021-04-08 04:22:20 +0900

    [add] DBの作成と接続

---
+++ libs/libdb.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import sqlite3
+
+
+def db_connection() -> sqlite3.Connection:
+    "閉じ忘れないように"
+    path = Path.cwd().glob('**/info.db')
+    db_name = str(list(path)[0])
+    conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES)
+    return conn
+
+
+def db_create():
+    db1 = """
+        CREATE TABLE IF NOT EXISTS e-mail_table (
+            discord_id  TEXT PRIMARY KEY,
+            e-mail      TEXT
+        );
+    """
+
+    db2 = """
+        CREATE TABLE IF NOT EXISTS token_table(
+            e-mail      TEXT PRIMARY KEY,
+            token       TEXT,
+            miss_count  INTEGER
+        );
+    """
+    conn = db_connection()
+    conn.execute(db1)
+    conn.execute(db2)
+    conn.commit()
+    conn.close()
