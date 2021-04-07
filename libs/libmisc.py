a5a75631b0fde8199d2421fe09ab3aed7f7bee8c
t4t5u0 <ymmtryk0902@gmail.com>
2021-04-08 03:22:36 +0900

    [add] コメント追加

--- libs/libmisc.py
+++ libs/libmisc.py
@@ -5,11 +5,13 @@ from datetime import datetime
 from hashlib import shake_128
 
 
-def create_token() -> str:
+def create_token(n=8: int) -> str:
+    "n桁のトークンを発行する関数"
     t = datetime.timestamp(datetime.now()).__int__().__str__().encode()
-    h = hashlib.sha256(t).hexdigest()[:8]
+    h = hashlib.sha256(t).hexdigest()[:n]
     return h
 
 
 def compare_token(target1: str, target2: str) -> bool:
+    "トークンを比較する関数"
     return hmac.compare_digest(target1, target2)
