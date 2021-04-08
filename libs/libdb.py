b244526fee7511ddccf9dce87ca1a28d5e53f0a3
t4t5u0 <ymmtryk0902@gmail.com>
2021-04-09 07:51:17 +0900

    [add] DB操作用の関数を仮実装

--- libs/libdb.py
+++ libs/libdb.py
@@ -1,6 +1,6 @@
-from pathlib import Path
-
 import sqlite3
+from pathlib import Path
+from datetime import datetime
 
 
 def db_connection() -> sqlite3.Connection:
@@ -34,17 +34,39 @@ def db_create():
     conn.close()
 
 
-def db_insert_email():
-    pass
+def db_insert_email(discord_id: str, email: str):
+    query = "INSERT INTO e-mail_table VALUES(?, ?)"
+
+    conn = db_connection()
+    conn.execute(query, (discord_id, email))
+    conn.commit()
+    conn.close()
 
 
-def db_delete_email():
-    pass
+def db_delete_email(discord_id: str):
+    query = "DELETE FROM e-mail_table WHERE discord_id = ?"
+    conn = db_connection()
+    conn.execute(query, (discord_id,))
+    conn.commit()
+    conn.close()
 
 
-def db_insert_token():
-    pass
+def db_insert_token(email: str, token: str):
+    query = "INSERT INTO token_table VALUES(?, ?, ?, ?)"
+    created_at = datetime.timestamp(datetime.now()).__int__()
+    miss_count = 0
+    conn = db_connection()
+    conn.execute(query, (email, token, created_at, miss_count))
+    conn.close()
 
 
-def db_delete_token():
+def db_delete_token(email: str):
+    query = "DELETE FROM e_mail_table WHERE email = ?"
+    conn = db_connection()
+    conn.execute(query, email)
+    conn.commit()
+    conn.close()
+
+
+def miss_count_up(discord_id: str):
     pass
