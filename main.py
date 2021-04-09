4fc9e105b2b98caddfb2dc5256e39963bcf432bd
t4t5u0 <ymmtryk0902@gmail.com>
2021-04-10 02:47:35 +0900

    [add] 関数の中に押し込めるの良くない気がしてきた

--- main.py
+++ main.py
@@ -5,24 +5,22 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-def main():
 
-    path = Path.cwd().glob('**/config.ini')
-    config = configparser.ConfigParser()
-    config.read(path)
-    TOKEN = config['TOKEN']['token']
+path = Path.cwd().glob('**/config.ini')
+config = configparser.ConfigParser()
+config.read(path)
+TOKEN = config['TOKEN']['token']
 
-    prefix = '!'
-    bot = Bot(command_prefix=prefix)
-    bot.load_extension('cog.auth_bot')
+prefix = '!'
+bot = Bot(command_prefix=prefix)
+bot.load_extension('cog.auth_bot')
 
-    @bot.event
-    async def on_ready():
-        print('login')
-        await bot.change_presence(activity=discord.Game(f'{prefix}auth 学籍番号 名前')) 
 
-    bot.run(TOKEN)
+@bot.event
+async def on_ready():
+    print('login')
+    await bot.change_presence(activity=discord.Game(f'{prefix}auth 学籍番号 名前'))
 
 
 if __name__ == "__main__":
-    main()
+    bot.run(TOKEN)
